7637d226ce240522fe7b2b3c403be06b5726d9c3
Jonathan Raphaelson <jon@accidental.cc>
2024-08-19 23:42:40 -0600

    lint: rubocop

--- bootstrap/parser_spec.rb
+++ bootstrap/parser_spec.rb
@@ -9,29 +9,29 @@ RSpec::Matchers.define :be_symbol do |expected|
   # symbol is tag
   # hash with :tag, :value keys
   match_unless_raises do |actual|
-    raise "but got nil" if actual.nil?
-    raise "but is not an atom" unless actual.atom?
-    raise "but is not a symbol" unless actual.tag == :symbol
+    raise 'but got nil' if actual.nil?
+    raise 'but is not an atom' unless actual.atom?
+    raise 'but is not a symbol' unless actual.tag == :symbol
     raise "but is the symbol '#{expected}'" unless actual.value == expected
   end
 end
 
 RSpec.describe DenverBS::Parser do
+  subject(:result) { enum.first }
+
   let(:parser) { described_class.new(input) }
   let(:enum)   { parser.each }
 
-  subject(:result) { enum.first }
-
-  describe "example" do
+  describe 'example' do
     let(:input) { <<~CODE }
       (alpha beta (1 2 3) #xff . 10)
     CODE
 
-    its(:car) { is_expected.to be_symbol("alpha") }
-    its(:cdar) { is_expected.to be_symbol("beta") }
+    its(:car) { is_expected.to be_symbol('alpha') }
+    its(:cdar) { is_expected.to be_symbol('beta') }
 
-    it "formats as expected" do
-      expect(result.to_s).to eq("(alpha beta (1.0 2.0 3.0) 255 . 10.0)")
+    it 'formats as expected' do
+      expect(result.to_s).to eq('(alpha beta (1.0 2.0 3.0) 255 . 10.0)')
     end
   end
 end
